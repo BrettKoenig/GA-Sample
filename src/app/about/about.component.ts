b295adf2b0c2f8dd48d3d57e321a24169f45d48d
Brett Koenig <koenig.337@osu.edu>
2018-06-06 19:46:54 -0400

    step6.1: bootstrap added

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'about',
   templateUrl: './about.template.html',
-  styles: [`
-            h2 {color: green; }
-            ul {
-                font-size: 30px;
-                color: red;
-              }`]
+  styles: ['h2 {color: green; } ']
 })
 export class AboutComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DatepickerModule, ModalModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -12,7 +13,12 @@ import { InterestsComponent } from './about/interests/interests.component';
     InterestsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //the .forRoot() method will make the functionalities available in all components and modules of your project (global scope)
+    //WARNING: be careful doing this often, it will clog your global scope and slow down your app
+    //It is often bad practice, but makes sense for things like this
+    DatepickerModule.forRoot(),
+    ModalModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
