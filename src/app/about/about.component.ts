31ec3cb3ea95ee3027aaa064dc8f1161c38f807d
Brett Koenig <koenig.337@osu.edu>
2018-06-06 19:35:17 -0400

    Step5 Finished

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'about',
-  templateUrl: './about.template.html'
+  templateUrl: './about.template.html',
+  styles: [`
+            h2 {color: green; }
+            ul {
+                font-size: 30px;
+                color: red;
+              }`]
 })
 export class AboutComponent { }
