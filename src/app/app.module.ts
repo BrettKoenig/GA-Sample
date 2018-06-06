3d16751a5a4b7c037cd4664b7c9f2a715d610782
Brett Koenig <koenig.337@osu.edu>
2018-06-06 18:39:42 -0400

    Step1 Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutModule } from './about/about.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutModule
   ],
   imports: [
     BrowserModule
