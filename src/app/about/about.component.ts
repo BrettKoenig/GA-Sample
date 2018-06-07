9acd4e22262722226fc6aa7f39a243b306e596ee
Brett Koenig <koenig.337@osu.edu>
2018-06-07 18:09:33 -0400

    Step7 Finished

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './about.template.html',
   styles: ['img {max-height: 300px; } ']
 })
-export class AboutComponent { }
+export class AboutComponent { 
+  public name: string = 'Brett Koenig';
+  public almaMater: string = 'The Ohio State University';
+  public extraCurricularActivity: string = 'NEWPATH';
+  public jsExperience: number = 8;
+  public favoriteTechnology: string = 'JavaScript';
+  public hmbReason: string = `I chose HMB because I knew I wanted to learn the entire app infrastructure from front to back. HMB was an
+  option where I knew, through working on various projects, I would have the opportunity to do that. Additionally,
+  they value both work-life balance, and learning; investing in their people to push us to grow.`
+  public profilePicture: string = '../../assets/img/profile.JPG';
+}
