e2dad63b1667301048d3ffa359311ab9068b5db3
Brett Koenig <koenig.337@osu.edu>
2018-06-07 19:10:46 -0400

    Step10 Finished

--- src/app/about/interests/interests.component.ts
+++ src/app/about/interests/interests.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { InterestGroup } from '../../models/interestGroup';
 
 @Component({
   selector: 'interests',
   templateUrl: './interests.template.html',
   styles: ['img {max-height: 200px; } ']
 })
-export class InterestsComponent { }
+export class InterestsComponent { 
+  @Input() interest: InterestGroup
+}
