ebd084a0217ef2c33713af4951d4495c7150a0ac
Brett Koenig <koenig.337@osu.edu>
2018-06-09 15:56:59 -0400

    Step22 Finished

--- src/app/about/interests/interests.component.ts
+++ src/app/about/interests/interests.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { InterestGroup } from '../../models/interestGroup';
+import { Interest } from '../../models/interest';
 
 @Component({
   selector: 'interests',
@@ -10,8 +11,29 @@ export class InterestsComponent {
   @Input() interest: InterestGroup
   @Output() notify: EventEmitter<string> = new EventEmitter<string>();
 
-  searchString: string = '';
-  
+  //we need to make a copy, otherwise, we would lose original data and have no way to get the original
+  filteredInterestItems: Interest[];
+
+  //looks like C#
+  _searchString: string;
+  get searchString(): string {
+    return this._searchString;
+  }
+  set searchString(value: string) {
+    this._searchString = value;
+    this.filteredInterestItems = this.searchString ? this.filterItems(this.searchString) : this.interest.items;
+  }
+
+  ngOnInit() {
+    this.filteredInterestItems = this.interest.items;
+    this.searchString = '';
+  }
+
+  filterItems(searchString: string): Interest[] {
+    return this.interest.items.filter((interest: Interest) => interest.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
+
+
   onClick(name: string): void {
     this.notify.emit(name)
   }
